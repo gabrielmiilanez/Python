d55e6f941b7a801cc4c94aeee03314c3e81f8870
gabrielmiilanez <gabriel.milanez@outlook.com>
2025-11-17 13:38:09 -0300

    feat: exercicio 3

---
+++ operacoes/operacoes.py
@@ -0,0 +1,13 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+soma = a + b
+print(f"A soma entre {a} e {b} é igual a {soma}.")
+
+diferença = a - b
+print(f"A diferença entre {a} e {b} é igual a {diferença}.")
+
+produto = a * b
+print(f"O produto entre {a} e {b} é igual a {produto}.")
+
+print(soma, diferença, produto)
---
+++ operacoes/operacoes2.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input("Digite o primeiro numero: "))
+    b = int(input("Digite o segundo numero: "))
+    
+    divisao = a // b
+    floatDivisao = float(a/b)
+    
+    print(divisao)
+    print(floatDivisao)
